7498c89a106acf669fb868381d8c07f1bc553ade
abdulrahman <abdulrahmanahmad_979@outlook.com>
2022-09-21 19:55:13 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ------------------------------------------ */
-let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0]; //المصفوفة
+let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 99]; //المصفوفة
 
 let all_grades_div = document.getElementById("all-grades");
 let random_grade_div = document.getElementById("random-grade");
@@ -9,9 +9,15 @@ let random_grade_div = document.getElementById("random-grade");
 /*  الجزء الأول والثاني من التمرين */
 
 
+function addtodiv (name){
+    document.getElementById("all-grades").innerHTML += name+"  ";
+}
 
+grades.forEach(addtodiv)
 
-
+let randomindex= Math.floor(grades.length* Math.random());
+let num = grades [randomindex];
+document.getElementById("random-grade").innerHTML = num+" " 
 
 
 
@@ -26,12 +32,17 @@ let course = {
 }
 
 //Change studentsCount,track property here
+course.track= "web"
+course.studentsCount= 40
 
 
 
 //Console log here
-
+console.log( `Hello my name is (${course.name}), i'm in (${course.track}) at (${course.location})`)
 
 
 //Create student object here
-
+let student = {
+  name: "abdulrhman",
+  location : "Kuwait University"
+}
